54d1d916916ff7c8bb4b65e73cd4c13dace9d3ea
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-06-25 09:46:29 +0700

    initialize project

---
+++ main/java/com/krakenforce/app/TrainBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.krakenforce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainBookingSystemApplication.class, args);
+	}
+
+}
